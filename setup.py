18f665835e4866680c7aa9fb5685d3172e9defdd
LyudmilaB <luda.balakireva@gmail.com>
2023-08-24 16:43:12 +0000

    phyton3 updated code

--- setup.py
+++ setup.py
@@ -1,34 +1,106 @@
-__author__ = 'Lyudmila Balakireva'
+# -*- coding: utf-8 -*-
+#
+# This file is part of TimeGate.
+#
+# TimeGate is free software; you can redistribute it and/or modify
+# it under the terms of the  License; see LICENSE file for
+# more details.
+
+"""A Memento TimeGate."""
+
 import os
-try:
-    from setuptools import setup
-    from setuptools import find_packages
-except ImportError:
-    from distutils.core import setup
+import sys
+
+from setuptools import find_packages, setup
+
+#readme = open('README.rst').read()
+
+#tests_require = [
+#    'check-manifest>=0.25',
+#    'coverage>=4.0',
+#    'isort>=4.2.2',
+#    'pydocstyle>=1.0.0',
+#    'pytest-cache>=1.0',
+#    'pytest-cov>=1.8.0',
+#    'pytest-pep8>=1.0.6',
+#    'pytest>=2.8.0',
+#    'httpretty>=0.8.14',
+#    'mock>=2.0.0',
+#]
+
+extras_require = {
+    ':python_version<"3.0"': [
+        'ConfigParser>=3.3.0r2',
+    ],
+    'docs': [
+        'Sphinx>=1.4.2',
+    ],
+    'uwsgi': [
+        'uWSGI>=2.0.3',
+    ]
+ #   'tests': tests_require,
+}
+
+extras_require['all'] = []
+for key, reqs in extras_require.items():
+    if key[0] == ':':
+        continue
+    extras_require['all'].extend(reqs)
+
+setup_requires = [
+    'pytest-runner>=2.6.2',
+]
+
+install_requires = [
+    'LinkHeader>=0.4.3',
+    'lxml>=3.4.1',
+    'python-dateutil>=2.1',
+    'requests>=2.2.1',
+    'werkzeug<1',
+]
+
+packages = find_packages()
 
 
-def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+# Get the version string. Cannot be done with import!
+g = {}
+with open(os.path.join('timegate', 'version.py'), 'rt') as fp:
+    exec(fp.read(), g)
+    version = g['__version__']
 
-
-setup(name='timegate',
-      version='0.2.1',
-      author_email='ludab@lanl.gov',
-      url='',
-      download_url='',
-      description="A  Memento Proxy for Invenio",
-      long_description=read('README.md'),
-      packages=find_packages(exclude=['doc.*']),
-      keywords='proxy memento uwsgi python',
-      license='',
-      install_requires=[
-          'uWSGI>=2.0.3',
-          'ConfigParser>=3.3.0r2',
-          'python-dateutil>=2.1',
-          'requests>=2.2.1',
-          'werkzeug>=0.9.6'
-
-      ],
-      include_package_data=True,
-      zip_safe=False
-      )
+setup(
+    name='timegate',
+    version=version,
+    description=__doc__,
+    keywords='memento timegate',
+    author='LANL',
+    packages=packages,
+    zip_safe=False,
+    include_package_data=True,
+    platforms='any',
+    entry_points={
+        'timegate.handlers': [
+            'caltech = timegate.examples.caltech:InvenioHandler'
+        ],
+    },
+    extras_require=extras_require,
+    install_requires=install_requires,
+    setup_requires=setup_requires,
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
+)
---
+++ timegate/version.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of TimeGate.
+#
+# TimeGate is free software; you can redistribute it and/or modify
+# it under the terms of the License; see LICENSE file for
+# more details.
+
+"""Version information for TimeGate.
+
+This file is imported by ``timegate.__init__``, and parsed by
+``setup.py``.
+
+"""
+
+from __future__ import absolute_import, print_function
+
+__version__ = "1.0.0.dev2023"
